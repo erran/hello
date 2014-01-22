bd868ad853caba7c2cb9e17ec3253c922f5ea19b
Erran Carey <me@errancarey.com>
2014-01-21 23:08:25 -0600

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,10 +1,49 @@
 package main
 
 import (
-  "fmt"
-  "github.com/erran/newmath"
+	"fmt"
+	"net/http"
+  "encoding/json"
 )
 
 func main() {
-  fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+	fmt.Printf("[hello] Starting server at 8080...\n")
+
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func badRequestHandler(resp http.ResponseWriter, req *http.Request, statusCode int) {
+	header := resp.Header()
+	header["Content-Type"] = []string{"application/json"}
+
+  respMap := make(map[string]interface{})
+  respMap["message"] = http.StatusText(statusCode)
+
+  var scheme string
+  if req.URL.Scheme == "" {
+    scheme = "http"
+  }
+
+  // [todo] - link to written documentation
+  respMap["documentation_url"] = fmt.Sprintf("%s://%s/", scheme, req.Host)
+
+  respJSON, _ := json.Marshal(respMap)
+	fmt.Fprintf(resp, string(respJSON))
+}
+
+func rootHandler(resp http.ResponseWriter, req *http.Request) {
+  if req.URL.Path != "/" {
+    badRequestHandler(resp, req, http.StatusNotFound)
+    return
+  }
+
+	header := resp.Header()
+	header["Content-Type"] = []string{"application/json"}
+
+  respMap := make(map[string]interface{})
+  respMap["path"] = req.URL.Path
+
+  respJSON, _ := json.Marshal(respMap)
+	fmt.Fprintf(resp, string(respJSON))
 }
